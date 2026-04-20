d1d43edf407eaf482d45d5afb55adaf05c33d0c0
jdlopsal <jdlopsal@posgrado.upv.es>
2026-04-20 19:09:54 +0200

    Agregamos misma optimizacion conv2D a  MaxPool2D

--- modules/maxpool2d.py
+++ modules/maxpool2d.py
@@ -3,41 +3,30 @@ from modules.layer import Layer
 import numpy as np
 
 class MaxPool2D(Layer):
-    def __init__(self, kernel_size, stride):
+ def __init__(self, kernel_size, stride):
         self.kernel_size = kernel_size
         self.stride = stride
 
-    def forward(self, input, training=True):  # input: np.ndarray of shape [B, C, H, W]
-        self.input = input
-        B, C, H, W = input.shape
-        KH, KW = self.kernel_size, self.kernel_size
-        SH, SW = self.stride, self.stride
-
-        out_h = (H - KH) // SH + 1
-        out_w = (W - KW) // SW + 1
-
-        self.max_indices = np.zeros((B, C, out_h, out_w, 2), dtype=int)
-        output = np.zeros((B, C, out_h, out_w),dtype=input.dtype)
-
-        for b in range(B):
-            for c in range(C):
-                for i in range(out_h):
-                    for j in range(out_w):
-                        h_start = i * SH
-                        h_end = h_start + KH
-                        w_start = j * SW
-                        w_end = w_start + KW
-
-                        window = input[b, c, h_start:h_end, w_start:w_end]
-                        max_idx = np.unravel_index(np.argmax(window), window.shape)
-                        max_val = window[max_idx]
-
-                        output[b, c, i, j] = max_val
-                        self.max_indices[b, c, i, j] = (h_start + max_idx[0], w_start + max_idx[1])
-
-        return output
-
-    def backward(self, grad_output, learning_rate=None):
+ def forward(self, input, training=True):  
+    self.input = input   
+    B, C, H, W = input.shape
+    KH, KW = self.kernel_size, self.kernel_size
+    SH, SW = self.stride, self.stride
+    out_h = (H - KH) // SH + 1
+    out_w = (W - KW) // SW + 1
+   
+    output = np.full((B, C, out_h, out_w), -np.inf, dtype=input.dtype)
+    
+    # Para cada posición (ki, kj) del kernel, comparar con TODA la ventana de salida a la vez
+    for ki in range(KH):
+        for kj in range(KW):
+            # elementos en posición (ki, kj) 
+            slice_view = input[:, :, ki:ki + out_h*SH:SH, kj:kj + out_w*SW:SW]
+            # máximo elemento a elemento
+            output = np.maximum(output, slice_view)
+    
+    return output
+def backward(self, grad_output, learning_rate=None):
         B, C, H, W = self.input.shape
         grad_input = np.zeros_like(self.input, dtype=grad_output.dtype)
         out_h, out_w = grad_output.shape[2], grad_output.shape[3]
